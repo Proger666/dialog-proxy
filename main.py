f81dd15191275aadfa84ddfa92d577b9a1d7a3b6
Proger666 <knorra@mail.ru>
2018-03-15 15:13:10 +0300

    something for the masses

--- main.py
+++ main.py
@@ -23,7 +23,6 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import logging
 from flask import Flask
 
-
 app = Flask(__name__)
 import requests
 
@@ -31,6 +30,7 @@ import requests
 @app.route('/', methods=['POST, GET'])
 def index():
     return "Forbidden"
+
 
 # Enable logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -52,6 +52,13 @@ PROJECT_ID = "menuet-bf2b5"
 ########################
 MEMORY_DB = {'users_context': []}
 
+
+# Structure
+# 'user_id': user.id,
+# 'session': session,
+# 'timestamp': datetime.datetime.now().timestamp(),
+# 'lifetime': 30 * 60  # 30 min
+# 'last_location': None
 
 ########################
 
@@ -99,11 +106,12 @@ def help(bot, update):
     update.message.reply_text('Help!')
 
 
-def get_user_location(chat_id, bot, update):
+def ask_user_location(chat_id, bot, update):
+    get_user_session(update.message.from_user)
     location_keyboard = telegram.KeyboardButton(text="📌 Я здесь!", request_location=True, )
     custom_keyboard = [[location_keyboard]]
     reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard, one_time_keyboard=True, resize_keyboard=True)
-    bot.send_message(chat_id=chat_id, text="Тыкни на кнопочку",
+    bot.send_message(chat_id=chat_id, text="Тыкни на кнопочку, пожалуйста",
                      reply_markup=reply_markup)
     pass
 
@@ -135,79 +143,132 @@ def get_user_session(user):
         'user_id': user.id,
         'session': session,
         'timestamp': datetime.datetime.now().timestamp(),
-        'lifetime': 30 * 60  # 30 min
+        'lifetime': 30 * 60,  # 30 min
+        'last_location': None,
+        'last_msg': None
     })
     return session
 
 
-def get_food_for_user_with_loc(update, food):
-    user_location = 'test'
+def check_user_context(user):
+    for user_context in MEMORY_DB['users_context']:
+        if user_context['user_id'] == user.id:
+            if datetime.datetime.now().timestamp() - user_context['timestamp'] <= user_context['lifetime']:
+                return user_context['session']
+            else:
+                break
+    return None
+
+
+def get_user_last_location(user):
+    last_location = None
+    for user_context in MEMORY_DB['users_context']:
+        if user_context['user_id'] == user.id:
+            if 'location' in user_context:
+                return user_context['location']
+            else:
+                break
+    logger.warning('failed to find user location')
+    return None
+
+
+def get_food_for_user_with_loc(bot, update, food):
+    session = check_user_context(update.message.from_user)
+    if session is None:
+        ask_user_location(chat_id=update.message.chat_id, bot=bot, update=update)
+    # should not be None
+    user_location = get_from_memory_DB(update.message.from_user, 'last_location')
+
     payload = {'action': 'get_food_loc', 'token': MENUET_TOKEN,
                'user_id': update.message.from_user.id,
                'query': food}
     # send to backend
     r = requests.post(MENUET_API, json.dumps(payload), verify=False)
     if r.status_code != 200:
-        logger.warning("we failed to get data from menuet for food " + update.message.text + " chat_id:" + str(
-            update.message.chat_id) + " " + str(
-            update.message.from_user) + "user location is " + str(user_location))
-        return False
+        logger.warning("we failed to get data from menuet for food " + food + " chat_id:")
+        #return None
+    test_data = {'items': []}
+    for i in range(0, 3):
+        test_data['items'].append({
+            'item': random_word() + random_word(),
+            'ingrs': [random_word() for _ in range(3)],
+            'cost': "".join(random.choice('1234567890') for _ in range(int(random.choice('2345')))),
+            'place': 'http://4sq.com/1zUuio6'})
+
+    return test_data
+
+
+def add_to_memory_DB(user, key, value):
+    for item in MEMORY_DB['users_context']:
+        if item['user_id'] == user.id:
+            item[key] = value
+            break
+    pass
+
+
+def get_from_memory_DB(user, key):
+    for item in MEMORY_DB['users_context']:
+        if item['user_id'] == user.id:
+            return item[key]
     pass
 
 
 def echo(bot, update):
     """Echo the user message."""
     text = update.message.text
-    session = get_user_session(update.message.from_user)
     print('Current memory DB ' + str(MEMORY_DB))
+
     chat_id = update.message.chat_id
     logger.warning(text)
+    session = check_user_context(update.message.from_user)
+    # create new session if not exist
+    if session is None:
+        get_user_session(update.message.from_user)
+        add_to_memory_DB(user=update.message.from_user, key='last_msg', value=update.message.text)
+        ask_user_location(chat_id, bot, update)
+    else:
+        session = session
+
+        response = parse_query(bot, chat_id, session, update, get_from_memory_DB(update.message.from_user, 'last_msg'))
+        # From dialog flow
+        action = response['result']['action']
+        if action == 'input.welcome':
+            ask_user_location(chat_id, bot, update)
+            update.message.reply_text(
+                "Что бы ты хотел съесть ? Например, Ларису ивановну хо... салат с кунжутом хочу !")
+        elif action == 'get-food':
+            resp = get_food_for_user_with_loc(bot, update, response['result']['parameters']['food'])
+            if resp is None:  # not resp:
+                update.message.reply_text('Сорян, чет ничего найти не могу :( Может выберем что-то другое ?')
+            else:
+                # TODO: add response
+                # expected structure
+                # { item: <>
+                # ingrs: []
+                # cost: <>
+                # place: link to addr
+                #
+                for x in resp['items']:
+                    update.message.reply_markdown('*' + x['item'] + '*' + '    ' + '₽ ' + '*' + x['cost'] + '*' + '\n' +
+                                                  '_' + ",".join([y for y in x['ingrs']]) + '_' + '\n' +
+                                                  x['place'])
+                send_result_options_buttons(chat_id, bot)
+        # detect_intent_texts(
+        #     PROJECT_ID, session, text, lang_code)
+
+
+def parse_query(bot, chat_id, session, update, msg):
     ai = apiai.ApiAI(CLIENT_ACCESS_TOKEN)
-
     request = ai.text_request()
-
     request.lang = 'ru'  # optional, default value equal 'en'
-
     request.session_id = session
-
-    request.query = update.message.text
+    request.query = msg
     print(request.query)
     bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING)
     response = request.getresponse()
     response = json.loads(response.read())
     update.message.reply_text(response['result']['fulfillment']['speech'])
-    # From dialog flow
-    action = response['result']['action']
-    if action == 'input.welcome':
-        get_user_location(chat_id, bot, update)
-    elif action == 'get-food':
-        # resp = get_food_for_user_with_loc(update, response['result']['parameters'])
-        if 1 != 1:  # not resp:
-            update.message.reply_text('Сорян, чет ничего найти не могу :( Попробуй снова ?')
-        else:
-            # TODO: add response
-            # expected structure
-            # { item: <>
-            # ingrs: []
-            # cost: <>
-            # place: link to addr
-            #
-            test_date = {'items': []}
-            for i in range(0, 3):
-                test_date['items'].append({
-                    'item': random_word() + random_word(),
-                    'ingrs': [random_word() for _ in range(3)],
-                    'cost': "".join(random.choice('1234567890') for _ in range(int(random.choice('2345')))),
-                    'place': 'http://4sq.com/1zUuio6'})
-
-            for x in test_date['items']:
-                update.message.reply_markdown('*' + x['item'] + '*' + '    ' + '₽ ' + '*' + x['cost'] + '*' + '\n' +
-                                              '_' + ",".join([y for y in x['ingrs']]) + '_' + '\n' +
-                                              x['place'])
-            send_result_options_buttons(chat_id, bot)
-
-    # detect_intent_texts(
-    #     PROJECT_ID, session, text, lang_code)
+    return response
 
 
 def random_word():
@@ -232,14 +293,19 @@ def save_user_location(chat_id, user, user_location):
 
 
 def location(bot, update):
+    # get or create user session
+    get_user_session(update.message.from_user)
     user = update.message.from_user
     user_location = update.message.location
     logger.info("Location of %s: %f / %f", user.first_name, user_location.latitude,
                 user_location.longitude)
+    add_to_memory_DB(update.message.from_user, 'last_location', user_location)
     update.message.reply_text('Отлично!')
     if not save_user_location(update.message.chat_id, user, user_location):
         logger.error("Failed to save data to menuet")
-    update.message.reply_text("Что бы ты хотел съесть ? Например, Ларису ивановну хо... салат с кунжутом хочу !")
+    if get_from_memory_DB(user, 'last_msg') is not None:
+        echo(bot, update)
+        return
     return BIO
 
 
@@ -274,5 +340,4 @@ def main():
 
 
 if __name__ == '__main__':
-
     main()
