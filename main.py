956e7ead0fdbdd5f465ad867ace52bfb4d25241d
Proger666 <knorra@mail.ru>
2018-04-08 00:33:32 +0300

    something for the masses

--- main.py
+++ main.py
@@ -18,11 +18,11 @@ CLIENT_ACCESS_TOKEN = '114a4b10c66c46dca27ad6f63cdc2ced'
 
 class USER:
     LOCATION = 'last_location'
+    LAST_D_ACTION = 'last_dialog_action'
 
 
 # [START import_libraries]
 import uuid
-import requests
 import dialogflow
 import datetime
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
@@ -47,7 +47,7 @@ logger = logging.getLogger(__name__)
 LOCATION, BIO = range(2)
 
 ################### MENUET INTEGRATION ###########
-MENUET_API = "https://scorpa.ml/menuet/bot/api"
+MENUET_API = "https://127.0.0.1:8000/menuet/bot/api"
 MENUET_TOKEN = "ya29.c.El97BacMtcOVmFuQGwApUY-EikgCG8YtGI2C6oAt73Gd9AsHFBntwabQElx-9ZIQJSRvIBprUaRqDvHo2JHZCOTW8Z80u2FH8k9XlsL1Lqeg"
 ##################################################
 ### DIALOGFLOW ####
@@ -170,7 +170,6 @@ def check_user_context(user):
 
 
 def get_user_last_location(user):
-    last_location = None
     for user_context in MEMORY_DB['users_context']:
         if user_context['user_id'] == user.id:
             if 'location' in user_context:
@@ -193,12 +192,29 @@ def get_food_for_user_with_loc(bot, update, food):
     payload = {'action': 'get_food_loc', 'token': MENUET_TOKEN,
                'user_id': update.message.from_user.id,
                'query': food,
-               "location": user_location}
-    # send to backend
-    r = requests.post(MENUET_API, json.dumps(payload), verify=False)
+               "loc_lng": user_location['longitude'],
+               "loc_lat": user_location['latitude']}
+    # send query to backend
+    headers = {'Content-type': 'application/json'}
+    r = requests.post(MENUET_API, json.dumps(payload), verify=False, headers=headers)
     if r.status_code != 200:
-        logger.warning("we failed to get data from menuet for food " + food + " chat_id:")
-        # return None
+        logger.warning(
+            "we failed to get data from menuet for food " + food + " chat_id:" + str(update.message.chat_id) + str(
+                r.content))
+        return {'status':'error', 'msg':'menuet down'}
+    elif r.status_code == 200:
+        if len(r.content) == 0:
+            return None
+        else:
+            # deprecated
+            if r.content == b'no more':
+                return {'status':'ok','msg': 'no more'}
+            else:
+                data_received = json.loads(r.content)
+                return data_received
+
+    elif r.status_code == 500:
+        return {'msg': 500}
     test_data = {'items': []}
     for i in range(0, 3):
         test_data['items'].append({
@@ -221,8 +237,12 @@ def set_to_memory_DB(user, key, value):
 def get_from_memory_DB(user, key):
     for item in MEMORY_DB['users_context']:
         if item['user_id'] == user.id:
-            return item[key]
+            return item.get(key)
     pass
+
+
+def reply_nothing_found(update, bot):
+    update.message.reply_text('Сорян, чет ничего найти не могу :( Может выберем что-то другое ?')
 
 
 def echo(bot, update):
@@ -239,46 +259,88 @@ def echo(bot, update):
     if session is None:
         session = get_user_session(update.message.from_user)
         set_to_memory_DB(user=update.message.from_user, key='last_msg', value=update.message.text)
+
     # do we have msg from user or its a new request ?
-    last_msg = get_from_memory_DB(update.message.from_user, 'last_msg')
+    last_msg = update.message.text if update.message.text is not None else get_from_memory_DB(update.message.from_user,
+                                                                                              'last_msg')
     last_action = get_from_memory_DB(update.message.from_user, 'last_action')
-    if (last_msg == '' or last_msg is None) and last_action == 'location':
+
+    # If its new request -no last msg - ask what he wants ? Or parse existing msg
+    if (last_msg == '' or last_msg is None) and last_action == 'location' and update.message.text == '':
         update.message.reply_text('Что бы ты хотел съесть ? Например, Ларису ивановну хо... салат с кунжутом хочу !')
         set_to_memory_DB(update.message.from_user, 'last_action', '')
-
-    else:
-        response = parse_query(bot, chat_id, session, update, last_msg if last_msg != '' else text)
         set_to_memory_DB(update.message.from_user, 'last_msg', '')
-        # From dialog flow
-        action = response['result']['action']
-        if action == 'input.welcome':
-            set_to_memory_DB(update.message.from_user, 'last_msg', '')
+    #### PARSE QUESTION #######
+    response = parse_query(bot, chat_id, session, update, last_msg if last_msg != '' else text)
+    # From dialog flow get action
+    action = response['result']['action']
+
+    #### IS IT WELCOME REQUEST ?
+    if action != 'input.welcome':
+        # since we parsed question - remove last msg
+        set_to_memory_DB(update.message.from_user, 'last_msg', '')
+
+    ##### IS IT A LOOP ? ######
+    if action == 'input.welcome' and get_from_memory_DB(update.message.from_user, USER.LAST_D_ACTION) == 'input.welcome':
+        action = 'input.loop'
+        update.message.reply_text('Что бы ты хотел съесть ? Например, Ларису ивановну хо... салат с кунжутом хочу !')
+        # clean last D action because we used it
+        set_to_memory_DB(update.message.from_user, USER.LAST_D_ACTION, 'None')
+        set_to_memory_DB(update.message.from_user, 'last_action', 'None')
+
+        return
+    if action == 'input.welcome':
+        update.message.reply_text(response['result']['fulfillment']['speech'])
+        set_to_memory_DB(update.message.from_user, 'last_action', str(action))
+        set_to_memory_DB(update.message.from_user, USER.LAST_D_ACTION, str(action))
+
+        location = get_from_memory_DB(update.message.from_user, USER.LOCATION)
+        if location is None:
             ask_user_location(chat_id, bot, update)
-        elif action == 'get-food':
-            # do we know where user is ?
-            location = get_from_memory_DB(update.message.from_user, USER.LOCATION)
-            if location is None:
-                ask_user_location(chat_id, bot, update)
-                set_to_memory_DB(update.message.from_user, 'last_msg', text)
-                return
-            resp = get_food_for_user_with_loc(bot, update, response['result']['parameters']['food'])
-            if resp is None:  # not resp:
-                update.message.reply_text('Сорян, чет ничего найти не могу :( Может выберем что-то другое ?')
-            else:
-                # TODO: add response
-                # expected structure
-                # { item: <>
-                # ingrs: []
-                # cost: <>
-                # place: link to addr
-                #
-                for x in resp['items']:
-                    update.message.reply_markdown('*' + x['item'] + '*' + '    ' + '₽ ' + '*' + x['cost'] + '*' + '\n' +
-                                                  '_' + ",".join([y for y in x['ingrs']]) + '_' + '\n' +
-                                                  x['place'])
-                send_result_options_buttons(chat_id, bot)
-        # detect_intent_texts(
-        #     PROJECT_ID, session, text, lang_code)
+            set_to_memory_DB(update.message.from_user, 'last_msg', text)
+            return
+
+    elif action == 'get-food':
+        update.message.reply_text(response['result']['fulfillment']['speech'])
+        # do we know where user is ?
+        location = get_from_memory_DB(update.message.from_user, USER.LOCATION)
+        if location is None:
+            ask_user_location(chat_id, bot, update)
+            set_to_memory_DB(update.message.from_user, 'last_msg', text)
+            return
+        resp = get_food_for_user_with_loc(bot, update, response['result']['parameters']['food'])
+        if resp is None:  # not resp:
+            reply_nothing_found(update,bot)
+            # ALWAYS USE FUCKING GET!!!! not direct point to list name!!
+        elif len(resp) == 0:
+            reply_nothing_found(update, bot)
+        elif resp.get('status') == 'error':
+            reply_nothing_found(update,bot)
+            logger.error('MENUET RETURNED ERROR!!!! ' + str(get_from_memory_DB(update.message.from_user.id, 'last_msg')))
+
+        else:
+            # TODO: add response
+            # expected structure
+            # { item: <>
+            # ingrs: []
+            # cost: <>
+            # place: link to addr
+            #
+            if len(resp) == 0:
+                reply_nothing_found(update,bot)
+            elif resp['msg'] == 'no more':
+                update.message.reply_text('Больше ничего не нашли :(')
+            elif resp['msg'] == 500:
+                update.message.reply_text('У нас тут все умерло :( Ща починим, погоди')
+            for x in resp['items']:
+                update.message.reply_markdown(
+                    '*' + x['item'] + '*' + '    ' + '₽ ' + '*' + str(x['cost']) + '*' + '\n' +
+                    '_' + x['ingrs'] + '_' + '\n' +
+                    '*' + x['rest_name'] + '*' + '\n' +
+                    x['rest_addr'])
+            send_result_options_buttons(chat_id, bot)
+    # detect_intent_texts(
+    #     PROJECT_ID, session, text, lang_code)
 
 
 def parse_query(bot, chat_id, session, update, msg):
@@ -291,7 +353,6 @@ def parse_query(bot, chat_id, session, update, msg):
     bot.send_chat_action(chat_id=chat_id, action=telegram.ChatAction.TYPING)
     response = request.getresponse()
     response = json.loads(response.read())
-    update.message.reply_text(response['result']['fulfillment']['speech'])
     return response
 
 
